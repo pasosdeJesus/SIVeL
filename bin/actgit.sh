ed177680ed8030d397b79820487c37437b2996cb
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2012-07-13 22:06:22 -0500

    Sync to 1.1

--- bin/actgit.sh
+++ bin/actgit.sh
@@ -2,6 +2,7 @@ git fetch origin
 git merge origin/master
 rama=`git branch | grep "^*"`
 if (test "$rama" = "* master") then {
+    echo "rama=$rama"
     git fetch upstream
     git merge upstream/master
 } fi;
