b58fb7a9f36325fe538b65fb671cf2e0861e0552
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2012-07-13 22:15:36 -0500

    Moving development shell scripts to bin

--- bin/actgit.sh
+++ bin/actgit.sh
@@ -2,7 +2,6 @@ git fetch origin
 git merge origin/master
 rama=`git branch | grep "^*"`
 if (test "$rama" = "* master") then {
-    echo "rama=$rama"
     git fetch upstream
     git merge upstream/master
 } fi;
