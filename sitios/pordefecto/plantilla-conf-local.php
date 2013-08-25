27f9f71add87e25e3b9375bfd73dd16ef1b9c29d
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2013-08-25 18:15:46 -0500

    Plantillas

---
+++ sitios/pordefecto/plantilla-conf-local.php
@@ -0,0 +1,28 @@
+<?php
+// vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker fileencoding=utf-8:
+/**
+ * Variables de configuración locales relacionadas con servidor y fuentes.
+ * Basado en script de configuración http://structio.sourceforge.net/seguidor
+ *
+ * PHP version 5
+ *
+ * @category  SIVeL
+ * @package   SIVeL
+ * @author    Su nombre <sunombre@sucorreo.org>
+ * @copyright 2013 Dominio público. Sin garantías.
+ * @license   https://www.pasosdejesus.org/dominio_publico_colombia.html Dominio Público. Sin garantías.
+ * @link      http://sivel.sf.net
+ * Acceso: SÓLO DEFINICIONES
+ */
+
+global $dbclave;
+/** Clave del usuario ante el MBD */
+$dbclave = "miclave";
+
+global $dirserv;
+/** Directorio de fuentes en servidor web */
+$dirserv = "/htdocs/sivel/";
+
+/** Nivel de depuración **/
+$GLOBALS['DB_Debug'] = 0;
+
