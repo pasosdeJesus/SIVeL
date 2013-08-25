88b80d14f0dea44fbc8ac67b72785acaefecb20d
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2013-08-25 18:18:22 -0500

    Plantillas

---
+++ sitios/pordefecto/plantilla-conf_int-local.php
@@ -0,0 +1,30 @@
+<?php
+// vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker fileencoding=utf-8:
+/**
+ * Variables de configuración locales de la interfaz de usuario.
+ * Basado en script de configuración http://structio.sourceforge.net/seguidor
+ *
+ * PHP version 5
+ *
+ * @category  SIVeL
+ * @package   SIVeL
+ * @author    Su nombre <sucorreo@sucorreo.org>
+ * @copyright 2013 Dominio público. Sin garantías.
+ * @license   https://www.pasosdejesus.org/dominio_publico_colombia.html Dominio Público. Sin garantías.
+ * @link      http://sivel.sf.net
+ * Acceso: SÓLO DEFINICIONES
+ */
+
+/**
+ * Color del fondo de la ficha de captura en notacion HTML
+ * @global string $GLOBALS['ficha_color_fondo']
+ */
+$GLOBALS['ficha_color_fondo'] = '#EEE';
+
+/** Archivo HTML que se pone en el centro del menú principal
+ * Dejar '' si no hay
+ * @global string $GLOBALS['centro_principal']
+ */
+//$GLOBALS['centro_principal'] 
+//  = 'sitios/muyparticular/centro_principal-local-' . $lang . ".html";
+
