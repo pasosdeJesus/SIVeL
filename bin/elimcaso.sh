8d0137779e9545a7fdaea6cc821c19f914721222
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2013-12-17 14:47:22 -0500

    Elimina caso

--- bin/elimcaso.sh
+++ bin/elimcaso.sh
@@ -8,5 +8,8 @@ if (test "$c" = "") then {
 	exit 1;
 } fi;
 
-../../bin/psql.sh -c "DELETE FROM caso_funcionario WHERE id_caso='$c'";
+for nt in acto actocolectivo caso_funcionario caso_contexto caso_categoria_presponsable caso_presponsable antecedente_caso ubicacion antecedente_victima victima caso_region caso_frontera antecedente_comunidad comunidad_vinculoestado comunidad_profesion comunidad_filiacion comunidad_organizacion comunidad_rangoedad comunidad_sectorsocial victimacolectiva caso_ffrecuente caso_fotra anexo caso_etiqueta; do
+	echo $nt
+	../../bin/psql.sh -c "DELETE FROM $nt WHERE id_caso='$c';"
+done
 ../../bin/psql.sh -c "DELETE FROM caso WHERE id='$c'";
