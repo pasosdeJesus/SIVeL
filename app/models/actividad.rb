550fc2904e6ee4bcd3404ea7848a91383542b74b
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2014-01-29 09:19:21 -0500

    Rangos de edad en actividades

--- app/models/actividad.rb
+++ app/models/actividad.rb
@@ -1,5 +1,9 @@
 class Actividad < ActiveRecord::Base
-	has_many :actividadareas_actividad, :dependent => :delete_all
-	has_many :actividadareas, :through => :actividadareas_actividad
-	has_many :actividad_rangoedad, :dependent => :delete_all
+	has_many :actividadareas_actividad, dependent: :delete_all
+	has_many :actividadareas, through: :actividadareas_actividad
+	has_many :rangoedad, through: :actividad_rangoedad
+	accepts_nested_attributes_for :rangoedad,  reject_if: :all_blank
+	has_many :actividad_rangoedad, foreign_key: "actividad_id", dependent: :delete_all
+	accepts_nested_attributes_for :actividad_rangoedad, 
+    allow_destroy: true, reject_if: :all_blank
 end
