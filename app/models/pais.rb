3af8563342960e478b3702c6de1a17d9704d3e97
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2014-03-04 20:01:24 -0500

    pais

---
+++ app/models/pais.rb
@@ -0,0 +1,8 @@
+class Pais < ActiveRecord::Base
+	has_many :clase, foreign_key: "id_pais", validate: true
+	has_many :municipio, foreign_key: "id_pais", validate: true
+	has_many :persona, foreign_key: "id_pais", validate: true
+	has_many :ubicacion, foreign_key: "id_pais", validate: true
+	has_many :desplazamiento, foreign_key: "paisdecl", validate: true
+	has_many :victimasjr, foreign_key: "id_pais", validate: true
+end
