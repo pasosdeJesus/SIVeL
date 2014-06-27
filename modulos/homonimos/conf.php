74acf0b68d850e5e8beae5b6ff608894a7331145
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2014-06-27 13:04:08 -0500

    homonimos en menu

---
+++ modulos/homonimos/conf.php
@@ -0,0 +1,24 @@
+<?php
+// vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker fileencoding=utf-8:
+/**
+ * Variables de configuración del módulo
+ *
+ * PHP version 5
+ *
+ * @category  SIVeL
+ * @package   SIVeL
+ * @author    Vladimir Támara <vtamara@pasosdeJesus.org>
+ * @copyright 2012 Dominio público. Sin garantías.
+ * @license   https://www.pasosdejesus.org/dominio_publico_colombia.html Dominio Público. Sin garantías.
+ * @link      http://sivel.sf.net
+ * Acceso: SÓLO DEFINICIONES
+ */
+
+// Opciones del menú
+$GLOBALS['modulo'][1026] = "modulos/homonimos/homonimos.php";
+$GLOBALS['m_opcion'][609] = array(
+    'nombre' => _('Homónimos'),
+    'idpapa' => 60,
+    'url' => 'opcion?num=1026',
+);
+
