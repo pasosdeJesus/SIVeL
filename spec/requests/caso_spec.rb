7f4b8afc8d15d3f2a02c3f9b40db8ef6ef06c8ed
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2014-08-04 10:16:10 -0500

    estructura

--- spec/requests/caso_spec.rb
+++ spec/requests/caso_spec.rb
@@ -2,9 +2,11 @@ require 'spec_helper'
 
 describe "Casos" do
 	describe "GET /caso" do
-		it "works! (now write some real specs)" do
+		it "muestra nuevos casos" do
+			Caso.create!(:memo => 'memo', :fecha => '2014-01-01')
 			get casos_path
 			response.status.should be(200)
+			response.body.should include("2014-01-01")
 		end
 	end
 end
