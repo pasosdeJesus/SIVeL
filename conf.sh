492da4d04e7e590df605947a8d06b488611f3f25
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2014-12-10 08:09:15 -0500

    doc

--- conf.sh
+++ conf.sh
@@ -259,7 +259,7 @@ verphp=`$PHP -v | grep "PHP " | sed -e "s|.*PHP \([0-9.]*\).*|\1|g"`;
 if (test "$?" != 0 -o "x$verphp" = "x") then {
 	echo "  $PROYECTO funciona con versiones de PHP posteriores a la 5.0";
 } fi;
-check "PHPDOC" "optional" "test -x \$PHPDOC" `which phpdoc 2> /dev/null`
+check "phpdoc" "optional" "test -x phpdoc" `which phpdoc 2> /dev/null`
 check "PEAR" "" "test -x \$PEAR" `which pear 2> /dev/null`
 echo " Paquetes de PEAR";
 estapear Auth
